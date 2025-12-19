6df252ad3e00a1165eda92b16a60c6c6ae6a84c2
vanshaj2023 <kumar.vanshaj2023@gmail.com>
2025-12-20 00:41:38 +0530

    fixed errors

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -14,7 +14,7 @@ type Props = {
 };
 
 const ProductDetails = async ({ params: { id } }: Props) => {
-  const product: Product = await getProductById(id);
+  const product = await getProductById(id);
   if (!product) redirect("/");
 
   const similarProducts = await getSimilarProducts(id);
